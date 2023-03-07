bee47830740e61eb2a788aee1958b2b3273fcde0
zTzyrant <61049884+zTzyrant@users.noreply.github.com>
2023-03-07 08:11:30 +0800

    Cleaning Unused class

--- src/app/secure/auth/dev.guard.ts
+++ src/app/secure/auth/dev.guard.ts
@@ -1,16 +1,12 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import { CurdApiService } from '../curd.api.service';
-import { DevService } from './dev.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DevGuard implements CanActivate {
   constructor(
-    private curdService: CurdApiService,
-    private devServices: DevService,
     private router: Router
   ){  }
   retValue = false
